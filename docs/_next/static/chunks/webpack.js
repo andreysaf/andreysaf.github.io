3f764fee6c1ea404c95994c267dd054146322d79
Andrey <a.so777@live.ca>
2020-09-06 16:25:05 -0600

    chore: regen site

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d5d537318dbe888fb224";
+/******/ 	var hotCurrentHash = "3f2ec17512279a7d0e25";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
