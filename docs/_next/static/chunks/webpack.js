01a14e2de30c10ca561cde7f4212e0880330e681
Andrey Safonov <a.so777@live.ca>
2021-12-28 10:38:21 -0800

    chore: regen site

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9121f13a9962ff03e765";
+/******/ 	var hotCurrentHash = "b75385bb9b2655637ace";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/development/_buildManifest.js
+++ docs/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/blog":["static\u002Fchunks\u002Fpages\u002Fblog.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cases":["static\u002Fchunks\u002Fpages\u002Fcases.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
